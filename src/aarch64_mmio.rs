c35a7c0da5e89d1fbba21f72ea680ef5c263cb46
Oliver Steffen <osteffen@redhat.com>
2026-06-19 16:28:36 +0200

    aarch64: Implement MmioOps trait for inline-assembly backend
    Implement MmioOps for the aarch64 backend, delegating to the existing
    inline-assembly functions. This replaces the duplicated mmio_read(),
    write_slice(), and read_slice() free functions with the shared trait
    implementations.
    
    No functional change.
    
    Signed-off-by: Oliver Steffen <osteffen@redhat.com>

--- src/aarch64_mmio.rs
+++ src/aarch64_mmio.rs
@@ -2,64 +2,74 @@
 // This project is dual-licensed under Apache 2.0 and MIT terms.
 // See LICENSE-APACHE and LICENSE-MIT for details.
 
+use crate::backend::mmio_ops::MmioOps;
 use crate::{SharedMmioPointer, UniqueMmioPointer};
-use core::ptr::NonNull;
 use zerocopy::{FromBytes, Immutable, IntoBytes};
 
-macro_rules! asm_mmio {
-    ($t:ty, $read_name:ident, $read_assembly:literal, $write_name:ident, $write_assembly:literal) => {
-        unsafe fn $read_name(ptr: *const $t) -> $t {
-            let value;
-            unsafe {
-                core::arch::asm!(
-                    $read_assembly,
-                    value = out(reg) value,
-                    ptr = in(reg) ptr,
-                );
-            }
-            value
+macro_rules! asm_read {
+    ($ins:literal, $reg:literal, $src:expr) => {{
+        let value;
+        // SAFETY: Caller guarantees src is valid and aligned.
+        unsafe {
+            core::arch::asm!(
+                concat!($ins, " {value:", $reg, "}, [{ptr}]"),
+                value = out(reg) value,
+                ptr = in(reg) $src,
+            );
         }
+        value
+    }};
+}
 
-        unsafe fn $write_name(ptr: *mut $t, value: $t) {
-            unsafe {
-                core::arch::asm!(
-                    $write_assembly,
-                    value = in(reg) value,
-                    ptr = in(reg) ptr,
-                );
-            }
+macro_rules! asm_write {
+    ($ins:literal, $reg:literal, $dst:expr, $value:expr) => {
+        // SAFETY: Caller guarantees dst is valid and aligned.
+        unsafe {
+            core::arch::asm!(
+                concat!($ins, " {value:", $reg, "}, [{ptr}]"),
+                value = in(reg) $value,
+                ptr = in(reg) $dst,
+            );
         }
     };
 }
 
-asm_mmio!(
-    u8,
-    read_u8,
-    "ldrb {value:w}, [{ptr}]",
-    write_u8,
-    "strb {value:w}, [{ptr}]"
-);
-asm_mmio!(
-    u16,
-    read_u16,
-    "ldrh {value:w}, [{ptr}]",
-    write_u16,
-    "strh {value:w}, [{ptr}]"
-);
-asm_mmio!(
-    u32,
-    read_u32,
-    "ldr {value:w}, [{ptr}]",
-    write_u32,
-    "str {value:w}, [{ptr}]"
-);
-asm_mmio!(
-    u64,
-    read_u64,
-    "ldr {value:x}, [{ptr}]",
-    write_u64,
-    "str {value:x}, [{ptr}]"
-);
+/// MmioOps backend using aarch64 inline assembly for MMIO access.
+struct Ops;
+
+impl MmioOps for Ops {
+    unsafe fn read_u8(src: *const u8) -> u8 {
+        asm_read!("ldrb", "w", src)
+    }
+
+    unsafe fn read_u16(src: *const u16) -> u16 {
+        asm_read!("ldrh", "w", src)
+    }
+
+    unsafe fn read_u32(src: *const u32) -> u32 {
+        asm_read!("ldr", "w", src)
+    }
+
+    unsafe fn read_u64(src: *const u64) -> u64 {
+        asm_read!("ldr", "x", src)
+    }
+
+    unsafe fn write_u8(dst: *mut u8, value: u8) {
+        asm_write!("strb", "w", dst, value);
+    }
+
+    unsafe fn write_u16(dst: *mut u16, value: u16) {
+        asm_write!("strh", "w", dst, value);
+    }
+
+    unsafe fn write_u32(dst: *mut u32, value: u32) {
+        asm_write!("str", "w", dst, value);
+    }
+
+    unsafe fn write_u64(dst: *mut u64, value: u64) {
+        asm_write!("str", "x", dst, value);
+    }
+}
 
 impl<T: FromBytes + IntoBytes> UniqueMmioPointer<'_, T> {
     /// Performs an MMIO read and returns the value.
@@ -74,7 +84,8 @@ impl<T: FromBytes + IntoBytes> UniqueMmioPointer<'_, T> {
     ///
     /// This field must be safe to perform an MMIO read from.
     pub unsafe fn read_unsafe(&mut self) -> T {
-        unsafe { mmio_read(self.regs) }
+        // SAFETY: self.regs is always a valid and unique pointer to MMIO address space.
+        unsafe { Ops::read(self.regs) }
     }
 }
 
@@ -88,12 +99,9 @@ impl<T: Immutable + IntoBytes> UniqueMmioPointer<'_, T> {
     ///
     /// This field must be safe to perform an MMIO write to.
     pub unsafe fn write_unsafe(&mut self, value: T) {
-        match size_of::<T>() {
-            1 => unsafe { write_u8(self.regs.cast().as_ptr(), value.as_bytes()[0]) },
-            2 => unsafe { write_u16(self.regs.cast().as_ptr(), convert(value)) },
-            4 => unsafe { write_u32(self.regs.cast().as_ptr(), convert(value)) },
-            8 => unsafe { write_u64(self.regs.cast().as_ptr(), convert(value)) },
-            _ => unsafe { write_slice(self.regs.cast(), value.as_bytes()) },
+        // SAFETY: self.regs is always a valid and unique pointer to MMIO address space.
+        unsafe {
+            Ops::write(self.regs, value);
         }
     }
 }
@@ -109,77 +117,7 @@ impl<T: FromBytes + IntoBytes> SharedMmioPointer<'_, T> {
     /// This field must be safe to perform an MMIO read from, and doing so must not cause any
     /// side-effects.
     pub unsafe fn read_unsafe(&self) -> T {
-        unsafe { mmio_read(self.regs) }
-    }
-}
-
-/// Performs an MMIO read and returns the value.
-///
-/// # Safety
-///
-/// The pointer must be valid to perform an MMIO read from.
-unsafe fn mmio_read<T: FromBytes + IntoBytes>(ptr: NonNull<T>) -> T {
-    match size_of::<T>() {
-        1 => convert(unsafe { read_u8(ptr.cast().as_ptr()) }),
-        2 => convert(unsafe { read_u16(ptr.cast().as_ptr()) }),
-        4 => convert(unsafe { read_u32(ptr.cast().as_ptr()) }),
-        8 => convert(unsafe { read_u64(ptr.cast().as_ptr()) }),
-        _ => {
-            let mut value = T::new_zeroed();
-            unsafe { read_slice(ptr.cast(), value.as_mut_bytes()) };
-            value
-        }
-    }
-}
-
-fn convert<T: Immutable + IntoBytes, U: FromBytes>(value: T) -> U {
-    U::read_from_bytes(value.as_bytes()).unwrap()
-}
-
-unsafe fn write_slice(ptr: NonNull<u8>, slice: &[u8]) {
-    if let Some((first, rest)) = slice.split_at_checked(8) {
-        unsafe {
-            write_u64(ptr.cast().as_ptr(), u64::read_from_bytes(first).unwrap());
-            write_slice(ptr.add(8), rest);
-        }
-    } else if let Some((first, rest)) = slice.split_at_checked(4) {
-        unsafe {
-            write_u32(ptr.cast().as_ptr(), u32::read_from_bytes(first).unwrap());
-            write_slice(ptr.add(4), rest);
-        }
-    } else if let Some((first, rest)) = slice.split_at_checked(2) {
-        unsafe {
-            write_u16(ptr.cast().as_ptr(), u16::read_from_bytes(first).unwrap());
-            write_slice(ptr.add(2), rest);
-        }
-    } else if let [first, rest @ ..] = slice {
-        unsafe {
-            write_u8(ptr.as_ptr(), *first);
-            write_slice(ptr.add(1), rest);
-        }
-    }
-}
-
-unsafe fn read_slice(ptr: NonNull<u8>, slice: &mut [u8]) {
-    if let Some((first, rest)) = slice.split_at_mut_checked(8) {
-        unsafe {
-            read_u64(ptr.cast().as_ptr()).write_to(first).unwrap();
-            read_slice(ptr.add(8), rest);
-        }
-    } else if let Some((first, rest)) = slice.split_at_mut_checked(4) {
-        unsafe {
-            read_u32(ptr.cast().as_ptr()).write_to(first).unwrap();
-            read_slice(ptr.add(4), rest);
-        }
-    } else if let Some((first, rest)) = slice.split_at_mut_checked(2) {
-        unsafe {
-            read_u16(ptr.cast().as_ptr()).write_to(first).unwrap();
-            read_slice(ptr.add(2), rest);
-        }
-    } else if let [first, rest @ ..] = slice {
-        unsafe {
-            *first = read_u8(ptr.as_ptr());
-            read_slice(ptr.add(1), rest);
-        }
+        // SAFETY: self.regs is always a valid pointer to MMIO address space.
+        unsafe { Ops::read(self.regs) }
     }
 }
--- src/backend/mod.rs
+++ src/backend/mod.rs
@@ -2,5 +2,5 @@
 // This project is dual-licensed under Apache 2.0 and MIT terms.
 // See LICENSE-APACHE and LICENSE-MIT for details.
 
-#[cfg(feature = "custom-mmio")]
+#[cfg(any(target_arch = "aarch64", feature = "custom-mmio"))]
 pub mod mmio_ops;
